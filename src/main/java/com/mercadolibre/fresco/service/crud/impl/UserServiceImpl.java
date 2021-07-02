0515b0c9e6ef6a502d00ba15cd262fa8f370d2ee
Danilo Peixoto Ferreira <danilo.peixoto@mercadolivre.com>
2021-07-02 14:15:36 -0300

    Add preauthorize annotation

--- src/main/java/com/mercadolibre/fresco/service/crud/impl/UserServiceImpl.java
+++ src/main/java/com/mercadolibre/fresco/service/crud/impl/UserServiceImpl.java
@@ -1,13 +1,29 @@
 package com.mercadolibre.fresco.service.crud.impl;
 
 import com.mercadolibre.fresco.model.User;
+import com.mercadolibre.fresco.repository.UserRepository;
 import com.mercadolibre.fresco.service.crud.IUserService;
+import com.mercadolibre.fresco.util.UserDetailsMapper;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
+@Service("userDetailsService")
 public class UserServiceImpl implements IUserService {
+    private static UserRepository userRepository;
+
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User retrievedUser = this.userRepository.findByUsername(username);
+
+        if (retrievedUser == null) {
+            throw new UsernameNotFoundException("Wrong username or password");
+        }
+
+        return UserDetailsMapper.build(retrievedUser);
+    }
+
     @Override
     public User create(User user) {
         return null;
