cacc4d3026c77457ccf8204abf8d2a341fc3d66d
José Raimundo <joseraimundo.barbosa@mercadolivre.com>
2021-07-05 16:08:54 -0300

    Warehouse code refactoring

--- src/main/java/com/mercadolibre/fresco/service/crud/impl/WarehouseServiceImpl.java
+++ src/main/java/com/mercadolibre/fresco/service/crud/impl/WarehouseServiceImpl.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.fresco.service.crud.impl;
 
+import com.mercadolibre.fresco.exceptions.NotFoundException;
 import com.mercadolibre.fresco.model.Warehouse;
 import com.mercadolibre.fresco.repository.WarehouseRepository;
 import com.mercadolibre.fresco.service.crud.IWarehouseService;
@@ -32,18 +33,38 @@ public class WarehouseServiceImpl implements IWarehouseService {
     }
 
     @Override
+    public Warehouse findWarehouseByCode(String code){
+        Warehouse warehouse = this.warehouseRepository.findWarehouseByCode(code);
+        if (warehouse == null){
+            throw new NotFoundException("Warehouse with code " + code + " not found!");
+        }
+        return warehouse;
+    }
+
+    @Override
     public Long getWarehouseIdByCode(String warehouseCode) {
         warehouseCode = warehouseCode.toUpperCase();
+        if (warehouseCode == null){
+            throw new NotFoundException("warehouse id not found!");
+        }
         return this.warehouseRepository.getWarehouseIdByCode(warehouseCode);
     }
 
     @Override
     public Warehouse findById(Long id) {
-        return this.warehouseRepository.findById(id).orElse(null);
+        Warehouse warehouse = this.warehouseRepository.findById(id).orElse(null);
+        if (warehouse == null){
+            throw new NotFoundException("Warehouse with id " + id + " not found!");
+        }
+        return warehouse;
     }
 
     @Override
     public List<Warehouse> findAll() {
-        return this.warehouseRepository.findAll();
+        List<Warehouse> warehouses = this.warehouseRepository.findAll();
+        if (warehouses.isEmpty()){
+            throw new NotFoundException("Warehouses not found!");
+        }
+        return warehouses;
     }
 }
